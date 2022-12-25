e82930ff8ebdb9e9ba7fe14ac11c5c3f51496b79
Yoshiharu Yamada <yoshiharuseijin@gmail.com>
2022-12-25 11:48:17 +0900

    Add view controller's implementation

--- Sources/EmojiPickerViewController/Controllers/SkintonePickerViewController.swift
+++ Sources/EmojiPickerViewController/Controllers/SkintonePickerViewController.swift
@@ -48,20 +48,127 @@ class SkintonePickerViewController: UIViewController {
     let emoji: Emoji
     
     /**
+     The size for a collectionview cell.
+     */
+    let itemSize: CGSize
+    
+    /**
+     The collection view for showing skintone variation emojis.
+     */
+    var collectionView: UICollectionView!
+    
+    /**
+     The diffable data source of the skintone variation's collection view.
+     
+     - The number of items is 5 when the type of emoji is person, such as 👮. (ex 👮🏻‍♀️👮🏼👮🏽👮🏾👮🏿)
+     - The number of items is 25 when the type of emoji is two people, such as 💏, due to 5 x 5 combination.
+     */
+    var dataSource: UICollectionViewDiffableDataSource<SkintonePickerSection, Emoji>!
+    
+    /**
      Initilizes this view controller by the given emoji.
      
-     - Parameter emoji: The emoji trying to pick a skintone variation.
+     - Parameters:
+         - emoji: The emoji trying to pick a skintone variation.
+         - itemSIze: The cell size of each skintone variation emoji.
      */
-    init(emoji: Emoji) {
+    init(emoji: Emoji, itemSize: CGSize = .init(width: 50, height: 50)) {
         precondition(emoji.hasVariations, "The emoji doesn't have any variation.")
         self.emoji = emoji
+        self.itemSize = itemSize
         super.init(nibName: nil, bundle: nil)
     }
     
+    @available(*, unavailable, message: "Must use init(emoji:itemSize)")
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        setupDataSource()
+        applyData()
+    }
+    
+}
+
+// MARK: - Adopting UICollectionView Delegate
+
+extension SkintonePickerViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // TODO: - Call handler
+        fatalError("Call Handler")
+    }
+    
+}
+
+// MARK: - Implementing Private Methods
+
+extension SkintonePickerViewController {
+    
+    private func setupView() {
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.minimumLineSpacing = 5
+        flowLayout.minimumInteritemSpacing = 5
+        flowLayout.sectionInset = .init(top: 0, left: 10, bottom: 0, right: 10)
+        flowLayout.itemSize = itemSize
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+        
+        view.backgroundColor = .secondarySystemFill
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+
+    }
+    
+    private func setupDataSource() {
+        
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewCell, Emoji> { cell, indexPath, item in
+            
+            var contentConfiguration = LabelContentConfiguration()
+            contentConfiguration.text = String(item.character)
+            contentConfiguration.font = UIFont.systemFont(ofSize: cell.bounds.height) // Presents the emoji as equal size as the parent cell.
+            contentConfiguration.textAlighment = .center
+
+            cell.contentConfiguration = contentConfiguration
+
+            cell.isAccessibilityElement = true
+            cell.accessibilityElements = []
+            cell.accessibilityTraits = .button
+            cell.accessibilityLabel = "\(item.textToSpeach)"
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<SkintonePickerSection, Emoji>(collectionView: collectionView, cellProvider: { [unowned emoji] collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: emoji)
+        })
+        
+    }
+    
+    private func applyData() {
+        var snapshot: NSDiffableDataSourceSnapshot<SkintonePickerSection, Emoji> = .init()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(emoji.orderedSkinToneEmojis, toSection: .main)
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+    
+}
+
+// MARK: - Defining Types
+
+enum SkintonePickerSection: Int {
+    case main
 }
 
 #endif
